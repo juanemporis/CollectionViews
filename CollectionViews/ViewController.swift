aeb6818153962fc88f393fefbcbb6ecf2f5a5b62
juan castañeda <juanemporis123>
2022-05-02 18:39:48 -0500

    Initial Commit

---
+++ CollectionViews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionViews
+//
+//  Created by wendy manrique on 2/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
